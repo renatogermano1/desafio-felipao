f9ed4c899b306efcdd06aedf506e9dc66e956b1c
renatogermano1 <renatohgermano@hotmail.com>
2023-12-22 18:49:16 -0300

    desafio heroi

---
+++ index.js
@@ -0,0 +1,30 @@
+//nivel classificador heroi
+
+let nomeHeroi = "Renatin"
+let xpHeroi = 7000
+
+if (xpHeroi < 1000){
+    nivelHeroi = "ferro"
+
+}else if (xpHeroi >= 1001 && xpHeroi <= 2000){
+    nivelHeroi = "bronze"
+
+}else if (xpHeroi >= 2001 && xpHeroi <= 5000){
+    nivelHeroi "prata"
+
+}else if (xpHeroi >= 6001 && xpHeroi <= 7000){
+    nivelHeroi "ouro"
+
+}else if (xpHeroi >= 7001 && xpHeroi <= 8000){
+    nivelHeroi "platina"
+
+}else if (xpHeroi >= 8001 && xpHeroi <= 9000){
+    nivelHeroi "ascendente"
+
+}else if (xpHeroi >= 9001 && xpHeroi <= 10000){
+    nivelHeroi "imirtal"
+
+}else { nivelHeroi = "platina"}
+
+console.log (nomeHeroi + xpHeroi);
+
